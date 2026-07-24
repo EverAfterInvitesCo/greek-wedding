6fcdcde7e7f9d5318b981f819f655bef1a2ffc4d
everafterinvites <contact.everafterinvites@gmail.com>
2026-07-25 02:06:23 +0300

    Update VenueSection.tsx

--- src/components/VenueSection.tsx
+++ src/components/VenueSection.tsx
@@ -99,7 +99,7 @@ export const VenueSection: React.FC = () => {
             <div className="relative flex-1 w-full min-h-[300px]">
               <iframe
                 title="Canaves Oia Resort Map"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3215.938830847761!2d25.3752520764126!3d36.46261597234479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzbCsDI3JzA1LjQiTiAyNcKwMjInMzkuMiJF!5e0!3m2!1sen!2s!4v1700000000000!5m2!1sen!2s"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.85!2d25.378!3d36.4618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1499122bc13d5cf3%3A0x4a1253503dc93cb1!2sCanaves%20Oia%20Luxury%20Resort!5e0!3m2!1sen!2sgr!4v1710000000000"
                 className="absolute inset-0 w-full h-full border-0 filter contrast-125 saturate-75 opacity-90"
                 allowFullScreen={false}
                 loading="lazy"
